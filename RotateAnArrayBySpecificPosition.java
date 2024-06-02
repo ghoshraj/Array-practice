2275e787422f8380aa952a7dd1f73ef5f5c08aa0
rajesh <110840094+ghoshraj@users.noreply.github.com>
2024-06-02 19:54:19 +0530

    First Commit

--- RotateAnArrayBySpecificPosition.java
+++ RotateAnArrayBySpecificPosition.java
@@ -4,14 +4,29 @@ public class RotateAnArrayBySpecificPosition {
     public static void main(String[] args) {
         int arr[] = {10,20,30,40,50,60};
         int index = 3;
+        int k=0;
         int b[] = new int[arr.length];
-        for (int i = 0; i < arr.length; i++){
-            if (i == index){
-                for (int j = arr.length - 1; j >= index; j--){
-
-                }
-            }
-            System.out.println(arr[i]);
+        for (int i = 0; i < index; i++){
+            b[k++] = arr[i];
         }
+        for(int i=arr.length-1;i>=index;i--){
+
+            b[k++]=arr[i];
+        }
+//
+
+        //Arrays.toString(arr);
+        //Arrays.toString(b);
+       // System.out.println(arr.toString());
+        for (int o:arr
+             ) {
+            System.out.print(o+" ");
+        }
+        System.out.println();
+        for (int o:b
+        ) {
+            System.out.print(o+ " ");
+        }
+
     }
 }
