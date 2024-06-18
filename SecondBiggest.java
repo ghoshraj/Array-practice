88101ffac52d36d8bfe0c87335aeeac4ccd67256
rajesh <110840094+ghoshraj@users.noreply.github.com>
2024-06-18 19:33:21 +0530

    First Commit

--- SecondBiggest.java
+++ SecondBiggest.java
@@ -2,7 +2,7 @@ package array;
 
 public class SecondBiggest {
     public static void main(String[] args) {
-        int arr[] = {10,20,30,60,40,50};
+        int arr[] = {1,1,5,8,9,9};
         int max = arr[0];
         int max1 = arr[0];
         for (int i = 0; i < arr.length; i++){
@@ -10,7 +10,7 @@ public class SecondBiggest {
                max1 = max;
                max = arr[i];
             }
-            else if (arr[i] > max1){
+            else if (arr[i] > max1 && arr[i] < max){
                 max1 = arr[i];
             }
         }
